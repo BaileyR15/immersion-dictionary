0d5f0a851774d44940a8d3dfa4c4cb9124f59647
Richard Bailey <rcb4by@virginia.edu>
2022-08-04 23:03:08 -0400

    Added database storing, removed old swa-func files

---
+++ api/db/index.js
@@ -0,0 +1,109 @@
+//@ts-check
+const axios = require("axios").default;
+const CosmosClient = require('@azure/cosmos').CosmosClient;
+
+const { endpoint, key, databaseId, containerId } = require('./config');
+
+const options = { endpoint, key };
+
+const client = new CosmosClient(options);
+const database = client.database(databaseId);
+const container = database.container(containerId);
+const items = container.items;
+module.exports = async function (context, req) {
+  if (req.method === 'POST') {
+  
+    const querySpec = {
+      query: "SELECT * FROM c WHERE c.id = @id",
+      parameters: [
+        {name: "@id", value: req.body.q}
+      ]
+    };
+
+    const it = await items.query(querySpec);
+    if (it.hasMoreResults()) {
+      const result = (await it.fetchNext()).resources[0];
+      await items.upsert({...result, accessed_at: [Date.now(), ...result.accessed_at]});
+
+      context.res = {
+        // status: 200, /* Defaults to 200 */
+        body: result.results
+      };
+      return;
+    }
+
+    let definitions = {};
+    try {
+      definitions = await axios.get(`${process.env.REACT_APP_OXFORD_ENDPOINT}/words/es`, {
+        headers: {
+          "Accept": "application/json",
+          "app_id": process.env.REACT_APP_OXFORD_APP_ID || '',
+          "app_key": process.env.REACT_APP_OXFORD_API_KEY || ''
+        },
+        params: {q: req.body.q}
+      });
+    } catch (error) {
+      if (error.response.status === 404) {
+        context.res = { body: [] }
+      }
+      else {
+        console.error(error);
+        context.res = error.response;
+      }
+      return;
+    }
+
+    let images = [];
+    try {
+      const resp = await axios.get(`${process.env.REACT_APP_BING_ENDPOINT}/v7.0/images/search`, {
+        headers: {"Ocp-Apim-Subscription-Key" : process.env.REACT_APP_BING_KEY_1 || ''}, 
+        params: {q: req.body.q}
+      });
+      images = resp.data.value.slice(0, 4).map(i => i.contentUrl);
+    } catch (error) {
+      console.error(error);
+      context.res = error.response;
+      return;
+    }
+
+    if (!Array.isArray(definitions.data.results)) {
+      context.res = {
+        body: []
+      }
+      return;
+    }
+
+    const remap = [];
+    for (var r of definitions.data.results) {
+      if (!Array.isArray(r.lexicalEntries)) continue;
+
+      for (var e of r.lexicalEntries) {
+        if (!Array.isArray(e.entries)) continue;
+
+        for (var n of e.entries) {
+          if (!Array.isArray(n.senses)) continue;
+
+          for (var s of n.senses) {
+            const toAdd = {
+              id: r.id,
+              word: r.word,
+              notes: e.notes,
+              definitions: s.definitions,
+              examples: s.examples,
+              images: images
+            };
+            remap.push(toAdd);
+          }
+        }
+      }
+
+    await items.upsert({id: req.body.q, accessed_at: [Date.now()], results: remap, "user": "richy"});
+
+    context.res = {
+      // status: 200, /* Defaults to 200 */
+      body: remap
+    };
+
+    }
+  }
+}
--- src/App.tsx
+++ src/App.tsx
@@ -14,47 +14,12 @@ function App(): JSX.Element {
   const getResults = async (event: React.FormEvent<HTMLFormElement>) => {
     event.preventDefault();
 
-    const definitions = await Axios.get("api/definitions", {params: {q: query}});
-    let images = []
     try {
-      const resp = await Axios.get("api/images", {params: {q: query}});
-      console.log(resp)
-      images = resp.data.value.slice(0, 4).map((i: {contentUrl: string}) => i.contentUrl);
+      const resp: { data: ResultViewProps[] } = await Axios.post("api/db", {q: query});
+      setResults(resp.data);
     } catch (error) {
       console.log(error);
     }
-
-    if (!Array.isArray(definitions.data.results)) return;
-    
-    const remap: ResultViewProps[] = [];
-    for (var r of definitions.data.results) {
-      if (!Array.isArray(r.lexicalEntries)) continue;
-
-      for (var e of r.lexicalEntries) {
-        if (!Array.isArray(e.entries)) continue;
-
-        for (var n of e.entries) {
-          if (!Array.isArray(n.senses)) continue;
-
-          for (var s of n.senses) {
-            const toAdd: ResultViewProps = {
-              id: r.id,
-              word: r.word,
-              notes: e.notes,
-              definitions: s.definitions,
-              examples: s.examples,
-              images: images
-            };
-            remap.push(toAdd);
-          }
-        }
-      }
-
-
-    }
-
-    setResults(remap);
-    
   };
 
   return (
